eb99fe748890cdba916a2e32c17411cffbf2ec9c
Kristopher William Goodell <73040875+krwigo@users.noreply.github.com>
2023-05-29 17:32:28 -0700

    set default to bootstrap

--- install.sh
+++ install.sh
@@ -4,15 +4,13 @@ PR="prettier"
 RE="react react-dom react-router-dom"
 WP="webpack webpack-cli webpack-dev-server css-loader style-loader file-loader esbuild-loader html-webpack-plugin"
 BS="bootstrap react-bootstrap react-bootstrap-icons"
-MU="@mui/material @mui/lab @mui/icons-material @emotion/react @emotion/styled"
+# MU="@mui/material @mui/lab @mui/icons-material @emotion/react @emotion/styled"
 BA="@babel/core @babel/node @babel/preset-env @babel/preset-react @babel/plugin-proposal-class-properties @babel/plugin-transform-strict-mode "
 
 npm i -D $ES $PR $WP
 npm i $RE $BS $MU $BA
 npm pkg set scripts.lint="prettier -w ."
 # npm pkg set scripts.build-esbuild="npm run lint && mkdir -p dist && esbuild frontend.js --bundle --outfile=dist/main.js --loader:.js=jsx"
-npm pkg set scripts.build-webpack="npm run lint && webpack build --config .webpack.config.js --mode production"
-npm pkg set scripts.build="npm run build-webpack"
-npm pkg set scripts.serve-webpack="webpack serve --config .webpack.config.js"
-npm pkg set scripts.serve="npm run serve-webpack"
+npm pkg set scripts.build="npm run lint && webpack build --config .webpack.config.js --mode production"
+npm pkg set scripts.serve="webpack serve --config .webpack.config.js"
 cat package.json
